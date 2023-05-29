f88797d3805947a96271eb18be4d83ec888f89d8
Felipe Baltor <fbaltor@gmail.com>
2023-05-29 19:34:53 -0300

    feat: extracting rules

--- sast.py
+++ sast.py
@@ -1,6 +1,8 @@
 import argparse
 import subprocess as sp
 import os
+import csv
+import json
 import tarfile
 import tempfile
 
@@ -10,14 +12,12 @@ from helpers import which
 class SAST():
 
     @staticmethod
-    def _run_semgrep(firmware_filesystem_directory_path):
+    def _run_semgrep(firmware_filesystem_directory_path, output_file):
         semgrep_exec = which('semgrep')
 
-        semgrep_output_file = tempfile.NamedTemporaryFile()
-
         result = sp.run(
-            [semgrep_exec, '--config=auto', firmware_filesystem_directory_path, '--verbose', '--json'],
-            capture_output = True,
+            [semgrep_exec, '--config=auto', firmware_filesystem_directory_path, '--no-git-ignore', '--json', '--output', output_file],
+            capture_output= True,
             text = True
         )
 
@@ -28,9 +28,9 @@ class SAST():
         return result
 
     @staticmethod
-    def run_semgrep(firmware_filesystem_path):
+    def run_semgrep(firmware_filesystem_path, output_file):
         if os.path.isdir(firmware_filesystem_path):
-            return SAST._run_semgrep(firmware_filesystem_path)
+            return SAST._run_semgrep(firmware_filesystem_path, output_file)
 
         if tarfile.is_tarfile(firmware_filesystem_path):
             with tarfile.open(firmware_filesystem_path) as tf:
@@ -38,15 +38,25 @@ class SAST():
                 extract_dir = os.path.join(extract_dir, 'extract')
                 tf.extractall(path = extract_dir)
 
-                return SAST._run_semgrep(extract_dir)
+                return SAST._run_semgrep(extract_dir, output_file)
             
-        return SAST._run_semgrep(firmware_filesystem_path)
+        return SAST._run_semgrep(firmware_filesystem_path, output_file)
 
     @staticmethod
     def extract_rules(firmware_filesystem_path, output_rules_path):
-        result = SAST.run_semgrep(firmware_filesystem_path)
-        print(result)
-        print(result.stdout)
+        with tempfile.NamedTemporaryFile() as tf:
+            SAST.run_semgrep(firmware_filesystem_path, tf.name)
+
+            with open(tf.name) as r:
+                semgrep_json = json.load(r)
+                results = semgrep_json['results']
+                rule_check_id_list = [result['check_id'] for result in results]
+
+            with open(output_rules_path, 'w') as w:
+                for rule in rule_check_id_list:
+                    w.write(f'{rule}\n')
+        
+        return rule_check_id_list
         
 def main():
     parser = argparse.ArgumentParser()
@@ -56,7 +66,7 @@ def main():
     args = parser.parse_args()
 
     if args.extract_rules:
-        return SAST.extract_rules(args.input, args.output)
+        return SAST.extract_rules(os.path.abspath(args.input), os.path.abspath(args.output))
 
 
 if __name__ == '__main__':
